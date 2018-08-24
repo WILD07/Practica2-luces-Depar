89dbc6b948faf559a2d55cc61750c3b430fe39cf
WILD07 <seg.wild07@gmail.com>
2018-08-24 12:20:34 -0400

    depar luces

---
+++ Practica VR Wilder Ramos/Assets/Scripts/Luces.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Luces : MonoBehaviour {
+
+    private Light myLight;
+    // Use this for initialization
+    void Start () {
+        myLight = GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider collision)
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+    }    
+}
